5561f3f2cacdcc23abd497578a11990a3ee39d6d
Zaklina Risteska <emil.risteski2@gmail.com>
2022-05-26 21:27:34 +0200

    Prv commit

---
+++ Zadaca48.EmilRisteskiINKI807/Zadaca48.EmilRisteskiINKI807/Zadaca48.EmilRisteskiINKI807.cpp
@@ -0,0 +1,17 @@
+// Zadaca48.EmilRisteskiINKI807.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string adresa;
+    const string sodrzina = "INKI807.Emil.Risteski";
+    cout << "Vnesete validna adresa na studentot: " << endl;
+    cin >> adresa;
+    cout << "\n" << sodrzina << " " << adresa << endl;
+}
+
+
