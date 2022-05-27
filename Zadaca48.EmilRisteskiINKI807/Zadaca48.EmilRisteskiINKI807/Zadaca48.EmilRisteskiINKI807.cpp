3de73849a24563fcb8144595435490153433b19d
Zaklina Risteska <emil.risteski2@gmail.com>
2022-05-27 02:46:28 +0200

    Podobruvanje na izgledot na kodot, dodaden vektor i datum na ragjanje

--- Zadaca48.EmilRisteskiINKI807/Zadaca48.EmilRisteskiINKI807/Zadaca48.EmilRisteskiINKI807.cpp
+++ Zadaca48.EmilRisteskiINKI807/Zadaca48.EmilRisteskiINKI807/Zadaca48.EmilRisteskiINKI807.cpp
@@ -1,17 +1,38 @@
-// Zadaca48.EmilRisteskiINKI807.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
+    vector<int>VectorASCII;
     string adresa;
-    const string sodrzina = "INKI807.Emil.Risteski";
+    string sodrzina2;
+
+    int denNaRagjanje = 20;
+    int mesecNaRagjanje = 8;
+    int godinaNaRagjanje = 2002;
+
+
+    const string sodrzina = "INKI807.Emil.Risteski"; 
+
     cout << "Vnesete validna adresa na studentot: " << endl;
-    cin >> adresa;
-    cout << "\n" << sodrzina << " " << adresa << endl;
+    cin >> adresa; 
+    cout << "\n" << sodrzina << " " << adresa << endl; 
+
+    sodrzina2 = sodrzina + adresa;
+
+
+    
+    for (int i = 0; i < sodrzina2.length(); i++) {
+        VectorASCII.push_back((int)sodrzina2[i]);
+    }
+    
+    cout << "Vektorot: " << endl;
+    for (int i = 0; i < VectorASCII.size(); i++) {
+        cout << VectorASCII[i] << " ";
+    }
+
+    cout << endl;
 }
-
-
