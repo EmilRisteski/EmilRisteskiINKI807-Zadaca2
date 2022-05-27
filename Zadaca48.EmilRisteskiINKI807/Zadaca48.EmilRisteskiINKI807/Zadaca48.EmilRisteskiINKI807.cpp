08db5a83a0d08d6d5f8f86f428e5ddd15d3f4eb6
Zaklina Risteska <emil.risteski2@gmail.com>
2022-05-27 02:54:52 +0200

    Deklariranje iterator, sortiranje na vektorot, dodavanje na datumot na ragjanje i dodadeni komentari

--- Zadaca48.EmilRisteskiINKI807/Zadaca48.EmilRisteskiINKI807/Zadaca48.EmilRisteskiINKI807.cpp
+++ Zadaca48.EmilRisteskiINKI807/Zadaca48.EmilRisteskiINKI807/Zadaca48.EmilRisteskiINKI807.cpp
@@ -15,24 +15,50 @@ int main()
     int godinaNaRagjanje = 2002;
 
 
-    const string sodrzina = "INKI807.Emil.Risteski"; 
+    const string sodrzina = "INKI807.Emil.Risteski"; // 1
 
     cout << "Vnesete validna adresa na studentot: " << endl;
-    cin >> adresa; 
-    cout << "\n" << sodrzina << " " << adresa << endl; 
+    cin >> adresa; // 2
+    cout << "\n" << sodrzina << " " << adresa << endl; // 3
 
     sodrzina2 = sodrzina + adresa;
 
 
-    
+    // 4
     for (int i = 0; i < sodrzina2.length(); i++) {
         VectorASCII.push_back((int)sodrzina2[i]);
     }
-    
+    // pecatenje na konzola
     cout << "Vektorot: " << endl;
     for (int i = 0; i < VectorASCII.size(); i++) {
         cout << VectorASCII[i] << " ";
     }
 
     cout << endl;
+
+    // 5
+     // deklariranje iterator za vektorot
+    vector<int>::iterator ptr;
+
+    // sortiranje vektor (najmal do najgolem)
+    sort(VectorASCII.begin(), VectorASCII.end());
+
+    // pecatenje sortiran vektor (go koristime iteratorot)
+    cout << "Sortiran vektor: ";
+    for (ptr = VectorASCII.begin(); ptr < VectorASCII.end(); ptr++) {
+        cout << *ptr << " ";
+    }
+
+    // 6 dodavanje na cifrite na datum na ragjanje  
+    VectorASCII.push_back(denNaRagjanje);
+    VectorASCII.push_back(mesecNaRagjanje);
+    VectorASCII.push_back(godinaNaRagjanje);
+
+    cout << "\nSortiran vektor i dodaden datumot na ragjanje : " << endl;
+    for (int i = 0; i < VectorASCII.size(); i++) {
+        cout << VectorASCII[i] << " ";
+    }
+
+
+    return 0;
 }
